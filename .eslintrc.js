73b5dc1041afb5a9ca77c629bdec8e73e324dfc6
Zbigniew Hadrian <zhadrian@unity.pl>
2018-10-26 21:57:22 +0200

    Removed uglify temporary

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "parserOptions": {
+        "sourceType": "module"
+    },extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/essential'
+  ],
+
+};
